6d06106b9828ce18ec56b1d5a28bb9ad9a80da56
OstapchukAB <electron.anton@gmail.com>
2021-12-27 15:36:28 +0300

    Init

---
+++ FormQR.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GenerateQRcode
+{
+    partial class FormQR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQRcreate = new System.Windows.Forms.Button();
+            this.pictureBoxQR = new System.Windows.Forms.PictureBox();
+            this.richTextBoxStrinfForQR = new System.Windows.Forms.RichTextBox();
+            this.buttonSaveQR = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonQRcreate
+            // 
+            this.buttonQRcreate.Location = new System.Drawing.Point(374, 230);
+            this.buttonQRcreate.Name = "buttonQRcreate";
+            this.buttonQRcreate.Size = new System.Drawing.Size(148, 43);
+            this.buttonQRcreate.TabIndex = 1;
+            this.buttonQRcreate.Text = "Создать QR";
+            this.buttonQRcreate.UseVisualStyleBackColor = true;
+            this.buttonQRcreate.Click += new System.EventHandler(this.buttonQRcreate_Click);
+            // 
+            // pictureBoxQR
+            // 
+            this.pictureBoxQR.Location = new System.Drawing.Point(12, 8);
+            this.pictureBoxQR.Name = "pictureBoxQR";
+            this.pictureBoxQR.Size = new System.Drawing.Size(346, 303);
+            this.pictureBoxQR.TabIndex = 2;
+            this.pictureBoxQR.TabStop = false;
+            // 
+            // richTextBoxStrinfForQR
+            // 
+            this.richTextBoxStrinfForQR.Location = new System.Drawing.Point(374, 8);
+            this.richTextBoxStrinfForQR.Name = "richTextBoxStrinfForQR";
+            this.richTextBoxStrinfForQR.Size = new System.Drawing.Size(312, 206);
+            this.richTextBoxStrinfForQR.TabIndex = 3;
+            this.richTextBoxStrinfForQR.Text = "";
+            // 
+            // buttonSaveQR
+            // 
+            this.buttonSaveQR.Location = new System.Drawing.Point(538, 230);
+            this.buttonSaveQR.Name = "buttonSaveQR";
+            this.buttonSaveQR.Size = new System.Drawing.Size(148, 43);
+            this.buttonSaveQR.TabIndex = 4;
+            this.buttonSaveQR.Text = "Сохранить QR";
+            this.buttonSaveQR.UseVisualStyleBackColor = true;
+            this.buttonSaveQR.Click += new System.EventHandler(this.buttonSaveQR_Click);
+            // 
+            // FormQR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 317);
+            this.Controls.Add(this.buttonSaveQR);
+            this.Controls.Add(this.richTextBoxStrinfForQR);
+            this.Controls.Add(this.pictureBoxQR);
+            this.Controls.Add(this.buttonQRcreate);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormQR";
+            this.Text = "Create QR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonQRcreate;
+        private System.Windows.Forms.PictureBox pictureBoxQR;
+        private System.Windows.Forms.RichTextBox richTextBoxStrinfForQR;
+        private System.Windows.Forms.Button buttonSaveQR;
+    }
+}
+
---
+++ FormQR.cs
@@ -0,0 +1,53 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateQRcode
+{
+    public partial class FormQR : Form
+    {
+        Bitmap qrCodeImage;
+        public FormQR()
+        {
+            InitializeComponent();
+            pictureBoxQR.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void buttonQRcreate_Click(object sender, EventArgs e)
+        {
+            _QRGenerate();
+        }
+
+        void _QRGenerate()
+        {
+            string txt = "";
+            qrCodeImage = null;
+
+            if (richTextBoxStrinfForQR.Text != null && richTextBoxStrinfForQR.Text.Length > 0)
+            {
+                txt = richTextBoxStrinfForQR.Text;
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(txt, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                qrCodeImage = qrCode.GetGraphic(20);
+                pictureBoxQR.Image = qrCodeImage;
+            }
+        }
+
+        private void buttonSaveQR_Click(object sender, EventArgs e)
+        {
+            if (qrCodeImage != null) {
+                qrCodeImage.Save($"{richTextBoxStrinfForQR.Text}.bmp");
+            
+            }
+        }
+    }
+}
