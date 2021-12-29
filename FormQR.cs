9d35ea16d5d76ef258bd321e1fc90437b5004d3c
OstapchukAB <electron.anton@gmail.com>
2021-12-29 16:20:16 +0300

    реализовал обход формирования матрицы QR кодов через вложенный цикл с необходимым приращением

--- FormQR.cs
+++ FormQR.cs
@@ -64,7 +64,9 @@ namespace GenerateQRcode
                 int w_text = w_img;
                 int h_text = 60;
                 int size_txt = 38;
-                using (Bitmap bmp = new Bitmap(2100, 2970))
+                int page_w = 2100;
+                int page_h = 2970;
+                using (Bitmap bmp = new Bitmap(page_w, page_h))
                 {
                    
                     using (Graphics g = Graphics.FromImage(bmp))
@@ -74,45 +76,25 @@ namespace GenerateQRcode
                         g.InterpolationMode = InterpolationMode.HighQualityBicubic;
                         g.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-                                            
-                        g.DrawImage(lstBitmap[0], 0,    0,     w_img, h_img);
-                        g.DrawString(  lstTxt[0],new Font("Tahoma", size_txt),Brushes.Black,new RectangleF(delta_x_text, delta_y_text, w_text, h_text));
-                        
-                        g.DrawImage(lstBitmap[1], 0,  650,     w_img, h_img);
-                        g.DrawString(  lstTxt[1], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(delta_x_text, 650+delta_y_text, w_text, h_text));
+                        int idx = 0;
+                        int delta_img_x = 650;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int delta_img_y = 650;
+                            for (int j = 0; j < 4; j++)
+                            {
+                                int x = i * delta_img_x;
+                                int y = j * delta_img_y;
+                                g.DrawImage(lstBitmap[idx], x, y, w_img, h_img);
+                                g.DrawString(lstTxt[idx], 
+                                    new Font("Tahoma", size_txt),
+                                    Brushes.Black,
+                                    new RectangleF(x+delta_x_text, y+delta_y_text, w_text, h_text));
+                                idx++;
+                            }
 
-                        g.DrawImage(lstBitmap[2], 0, 1300,     w_img, h_img);
-                        g.DrawString(  lstTxt[2], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(delta_x_text, 1300 + delta_y_text, w_text, h_text));
-                        
-                        g.DrawImage(lstBitmap[3], 0, 1950,     w_img, h_img);
-                        g.DrawString(  lstTxt[3], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(delta_x_text, 1950 + delta_y_text, w_text, h_text));
-
-                        //****************************
-                        g.DrawImage(lstBitmap[4], 650, 0,      w_img, h_img);
-                        g.DrawString(  lstTxt[4], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(650+delta_x_text, delta_y_text, w_text, h_text));
-                        
-                        g.DrawImage(lstBitmap[5], 650, 650,    w_img, h_img);
-                        g.DrawString(lstTxt[5], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(650 + delta_x_text, 650+delta_y_text, w_text, h_text));
-                        
-                        g.DrawImage(lstBitmap[6], 650, 1300,   w_img, h_img);
-                        g.DrawString(  lstTxt[6], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(650 + delta_x_text,1300+ delta_y_text, w_text, h_text));
-                        
-                        g.DrawImage(lstBitmap[7], 650, 1950,   w_img, h_img);
-                        g.DrawString(  lstTxt[7], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(650 + delta_x_text, 1950+delta_y_text, w_text, h_text));
-
-                        //*****************************************
-                        g.DrawImage(lstBitmap[8], 1300, 0,     w_img, h_img);
-                        g.DrawString(  lstTxt[8], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(1300 + delta_x_text, delta_y_text, w_text, h_text));
-
-                        g.DrawImage(lstBitmap[9], 1300, 650,   w_img, h_img);
-                        g.DrawString(  lstTxt[9], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(1300 + delta_x_text, 650+delta_y_text, w_text, h_text));
-
-                        g.DrawImage(lstBitmap[10], 1300, 1300, w_img, h_img);
-                        g.DrawString(  lstTxt[10], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(1300 + delta_x_text, 1300+delta_y_text, w_text, h_text));
-
-                        g.DrawImage(lstBitmap[11], 1300, 1950, w_img, h_img);
-                        g.DrawString(  lstTxt[11], new Font("Tahoma", size_txt), Brushes.Black, new RectangleF(1300 + delta_x_text, 1950+delta_y_text, w_text, h_text));
-
+                        }
+                      
                         g.Flush();
 
                     }
